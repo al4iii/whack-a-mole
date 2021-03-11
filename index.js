550e47a1e87fabb3718f6b0520a700ef0043bc9e
Ali Da <al4icrux@gmail.com>
2021-03-11 09:50:57 +0300

    refactor: apply prettier;

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
-const button = document.querySelector('.button');
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+const button = document.querySelector(".button");
 let lastHole;
 let timeUp = false;
 let score = 0;
 
-let randomTime = (min, max) => Math.round(Math.random() * (max - min) + min);
-
-let randomHole = (holes) => {
+const randomTime = (min, max) => Math.round(Math.random() * (max - min) + min);
+const randomHole = (holes) => {
   const idx = Math.floor(Math.random() * holes.length);
   const hole = holes[idx];
-  if (hole === lastHole) {
-    return randomHole(holes);
-  }
-  lastHole = hole;
-  return hole;
+  if (hole === lastHole) randomHole(holes)
+  return lastHole = hole; 
 };
-
-let peep = () => {
+const peep = () => {
   const time = randomTime(200, 1000);
   const hole = randomHole(holes);
-  hole.classList.add('up');
-  setTimeout (() => {
-    hole.classList.remove('up');
-    if (!timeUp) peep();
+  hole.classList.add("up");
+  setTimeout(() => { hole.classList.remove("up")
+   if (!timeUp) peep();
   }, time);
 };
-
-let startGame = () => {
+const startGame = () => {
   scoreBoard.textContent = 0;
   timeUp = false;
   score = 0;
   peep();
-  setTimeout(() => timeUp = true, 15000)
+  setTimeout(() => (timeUp = true), 15000);
 };
-
-let bonk = (e) => {
-  if(!e.isTrusted) return;
+const bonk = (e) => {
+  if (!e.isTrusted) return;
   score++;
-  e.target.parentNode.classList.remove('up');
+  e.target.parentNode.classList.remove("up");
   scoreBoard.textContent = score;
 };
-
-button.addEventListener('click', startGame);
-moles.forEach(mole => mole.addEventListener('click', bonk));
+button.addEventListener("click", startGame);
+moles.forEach((mole) => mole.addEventListener("click", bonk));
